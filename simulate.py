3b547c5ed9235dc25df6c5f1693ef80760269142
Nikolas Cruz-Marsted <nc376@cornell.edu>
2020-12-18 19:14:57 -0500

    small changes

--- simulate.py
+++ simulate.py
@@ -7,14 +7,14 @@ from randomBot import RandomBot
 import numpy as np
 
 if __name__ == '__main__':
-    blogger_bot = MontecarloBot()
+    montecarlo_bot = MontecarloBot()
     callbot = CallBot()
     nit = Nit()
     randombot = RandomBot()
 
     # Contains payout after each
     payouts = []
-    p1, p2 = randombot, blogger_bot
+    p1, p2 = randombot, montecarlo_bot
     for round in range(500):
         config = setup_config(
             max_round=5, initial_stack=100, small_blind_amount=2)
@@ -22,5 +22,5 @@ if __name__ == '__main__':
         config.register_player(name="bot2", algorithm=p2)
         game_result = start_poker(config, verbose=0)
         payouts.append([player['stack'] for player in game_result['players']
-                        if player['uuid'] == blogger_bot.uuid])
+                        if player['uuid'] == montecarlo_bot.uuid])
         print('Avg. Winnings:', '%d' % (int(np.mean(payouts))))
